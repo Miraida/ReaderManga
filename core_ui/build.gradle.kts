d168c00360a0cf45de45748ef0b5f75d3a01ab60
Mira <miraidajaanbaeva@gmail.com>
2023-01-23 13:43:09 +0600

    Add core_ui module

---
+++ core_ui/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    androidLibrary
+    jetbrainKotlinAndroid
+}
+
+android {
+    compileSdk = appConfig.compileSdkVersion
+
+    defaultConfig {
+        minSdk = appConfig.minSdkVersion
+        targetSdk = appConfig.targetSdkVersion
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(deps.androidx.coreKtx)
+    implementation(deps.androidx.appCompat)
+    implementation(deps.androidx.material)
+
+    /** Glide**/
+    implementation(deps.glide)
+}
